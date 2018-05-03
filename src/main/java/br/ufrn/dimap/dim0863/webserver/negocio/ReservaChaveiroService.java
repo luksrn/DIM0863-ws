86b2f7cca43461ec8277e85aaa9ff37b973c5883
Lucas Oliveira <luksrn@gmail.com>
2018-05-02 21:41:53 -0300

    INIT

--- src/main/java/br/ufrn/dimap/dim0863/webserver/negocio/ReservaChaveiroService.java
+++ src/main/java/br/ufrn/dimap/dim0863/webserver/negocio/ReservaChaveiroService.java
@@ -39,6 +39,9 @@ public class ReservaChaveiroService {
 				.findAny()
 				.orElseThrow( () -> new ChaveNaoDisponivelException() );
 		
+		// Notificarr Orion
+		//new NgsiClient().
+		
 		return new ChaveiroResponse( reserva.getChave() );
 	}
 	
